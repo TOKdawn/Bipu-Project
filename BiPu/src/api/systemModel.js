eaabb38bf9f853e324a3e3f9370542356080634e
Zhangpengyu <18746442232@163.com>
2021-04-07 11:03:06 +0800

    增加基础页面

--- BiPu/src/api/systemModel.js
+++ BiPu/src/api/systemModel.js
@@ -1,6 +1,15 @@
 import request from '/@/utils/request'
-let systemXHR = {};
-systemXHR.login = function(data) {
+let XHRList = {
+};
+const systemXHR = new Proxy(XHRList,{ //拦截未定义的URL请求
+    get(target, prop) {
+        if (prop in target) {
+            return target[prop]
+        }
+        throw new Error(`${prop} is error XHR request`);
+    }
+})
+XHRList.login = function(data) {
     return request({
         url: '/api/fin-services/v1/system-info/login',
         method: 'post',
---
+++ BiPu/src/pages/page.vue
@@ -0,0 +1,46 @@
+<template>
+  <div style="min-width:1024px;">
+    <Head :activeIndex="headData"></Head>
+    <router-view style="margin-top:60px;"></router-view>
+    <Foot></Foot>
+  </div>
+</template>
+<script>
+import Head from './layout/header'
+import Foot from './layout/footer'
+export default {
+  data () {
+    return {
+      headData: '10'
+    }
+  },
+  components: {
+    Head,
+    Foot
+  },
+  created () {
+    switch (this.$route.name) {
+      case 'volumelist':
+        this.headData = '2'
+        break
+      case 'articleList':
+        this.headData = '3'
+        break
+      case 'translator':
+        this.headData = '4'
+        break
+      case 'about':
+        this.headData = '5'
+        break
+      case 'download':
+        this.headData = '6'
+        break
+      default :
+        this.headData = '10'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "src/assets/style.scss";
+</style>
