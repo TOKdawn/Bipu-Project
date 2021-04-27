00c341ee209a121802f7e68141ab6dc89e2c2911
Zhangpengyu <18746442232@163.com>
2021-04-27 18:51:33 +0800

    外围组件引入

---
+++ BiPu/src/router/constant.ts
@@ -0,0 +1,3 @@
+export const REDIRECT_NAME  = 'Redirect' //页面重定向
+
+export const PARENT_LAYOUT_NAME = 'ParentLayout';
--- BiPu/src/router/index.ts
+++ BiPu/src/router/index.ts
@@ -1,4 +1,9 @@
+import type { RouteRecordRaw } from 'vue-router';
+import type { App } from 'vue';
+
 import { createRouter, createWebHistory } from 'vue-router'
+import { basicRoutes, LoginRoute } from './routes'; //引入路由模块
+import { REDIRECT_NAME } from './constant'; //超时重定向时间
 const routeMap = require('./routers') //引入路由表
 // vue2中使用 mode: history 实现
 const routerHistory = createWebHistory();
